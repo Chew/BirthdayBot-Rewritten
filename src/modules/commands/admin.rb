098a1b0b2357c18445aefb131c892adec40ac297
Chew <chew@chew.pw>
2019-09-15 09:09:40 -0500

    reset commands for admin

---
+++ src/modules/commands/admin.rb
@@ -0,0 +1,44 @@
+module Bot::DiscordCommands
+  # Admins 1
+  module Admin
+    extend Discordrb::Commands::CommandContainer
+    command(:reset) do |event, user|
+      unless Bot::BOT.server(621525131214192653).member(event.user.id).roles.include?(Bot::BOT.server(621525131214192653).role(621528165814370324))
+        event.respond "NO ADMIN? NO RESET!"
+        break
+      end
+
+      target = if user.nil?
+                 event.user
+               elsif !Bot::BOT.user(user.to_i).nil?
+                 Bot::BOT.user(user)
+               elsif !Bot::BOT.parse_mention(user).nil?
+                 Bot::BOT.parse_mention(user)
+               end
+
+      day = Birthday.find_by(userid: target.id)
+      if day.nil?
+        begin
+          event.channel.send_embed do |embed|
+            embed.title = "Birthday Information for #{target.distinct}"
+            embed.add_field(name: "Birthday", value: "No birthday set for this user!")
+            embed.color = 0xFF685F
+          end
+        rescue Discordrb::Errors::NoPermission
+          event.user.dm "I need Embed Links permissions to send messages"
+        end
+      else
+        day.destroy!
+        begin
+          event.channel.send_embed do |embed|
+            embed.title = "Birthday Updated for #{target.distinct}"
+            embed.add_field(name: "Birthday", value: "Cleared Birthday!")
+            embed.colour = 0xFFDF9C
+          end
+        rescue Discordrb::Errors::NoPermission
+          event.user.dm "I need Embed Links permissions to send messages"
+        end
+      end
+    end
+  end
+end
