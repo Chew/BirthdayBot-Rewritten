47a2bc3d9e47e62c6f3c76eba9a37ed963d8c381
Chew <chew@chew.pw>
2019-09-15 10:26:43 -0500

    pretty method in birthday model

--- src/models/birthday.rb
+++ src/models/birthday.rb
@@ -1,2 +1,17 @@
 class Birthday < ActiveRecord::Base
+  def pretty
+    gmt = offset
+    gmt = "+#{gmt}" if offset.positive?
+    if gmt.zero?
+      gmt = ''
+    end
+
+    formatstring = if hideyear
+                     "%B %-d"
+                   else
+                     "%B %-d, %Y"
+                   end
+
+    "#{birthday.strftime(formatstring)} GMT#{gmt}"
+  end
 end
