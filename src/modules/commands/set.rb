7720a4a92c331e6e5396b1ac2a65c75413b5ab22
Chew <chew@chew.pw>
2019-09-15 09:09:26 -0500

    pretty up set and add flip command

--- src/modules/commands/set.rb
+++ src/modules/commands/set.rb
@@ -5,7 +5,7 @@ module Bot::DiscordCommands
     command(:set, min_args: 1) do |event, *input|
       yourday = Birthday.find_by(userid: event.user.id)
       if yourday.nil?
-        m = event.respond "No birthday for that/you bucko, let';s set one!"
+        m = event.respond "No birthday for that/you bucko, let's set one!"
 
         year = 2019
         daye = nil
@@ -36,16 +36,57 @@ module Bot::DiscordCommands
           daye = date[1]
           gmt = date[2]
         else
-          event.respond "Yeah so big brain time. your format isn't supported. kinda sucks. anyway, like and subscribe and try a new format"
+          m.edit "Yeah so big brain time. your format isn't supported. kinda sucks. anyway, like and subscribe and try a new format"
           break
         end
 
         bigdeal = Time.new(year, month, daye, 0, 0, 0)
-        message = event.respond "Set your birthday to #{bigdeal.strftime("%B %-d, %Y")} with gmt big boy of GMT #{gmt}. Not what you meant? well wait until i add a way to make sure BTW IT WASNT SAVED THE DB COMMAND IS NO HERE"
-
-        nil
+        Birthday.create(userid: event.user.id, birthday: bigdeal, offset: gmt)
+        begin
+          m.delete
+          event.channel.send_embed do |embed|
+            embed.title = "Birthday Updated for #{event.user.distinct}"
+            embed.add_field(name: "Birthday", value: "Set to #{bigdeal.strftime("%B %-d, %Y")} GMT #{gmt}")
+            embed.footer = { text: "Meant day, month? Type bday flip" }
+            embed.color = 0xFFDF9C
+          end
+        rescue Discordrb::Errors::NoPermission
+          event.user.dm "I need Embed Links permissions to send messages"
+        end
       else
-        event.respond "You have a birthday set already... wtf."
+        event.respond "You have a birthday set already... See it with `bday get`."
+      end
+    end
+
+    command(:flip) do |event|
+      yourday = Birthday.find_by(userid: event.user.id)
+      if yourday.nil?
+        event.respond "You don't have a Birthday set, set one with `bday set`."
+      else
+        if yourday.flipped?
+          event.respond "You have already flipped your Birthday once. If you need help, go to the support server with `bday support`"
+          break
+        end
+
+        if (yourday.created.to_i - Time.now.to_i) > 600
+          event.respond "You set your Birthday too long ago and can no longer flip it. If you need help, go to the support server with `bday support`"
+          break
+        end
+
+        newday = Time.new(yourday.birthday.year, yourday.birthday.day, yourday.birthday.month)
+        yourday.birthday = newday
+        yourday.flipped = 1
+        yourday.save!
+
+        begin
+          event.channel.send_embed do |embed|
+            embed.title = "Birthday Updated for #{event.user.distinct}"
+            embed.add_field(name: "Birthday", value: "Set to #{newday.strftime("%B %-d, %Y")} GMT #{yourday.offset}")
+            embed.color = 0xFFDF9C
+          end
+        rescue Discordrb::Errors::NoPermission
+          event.user.dm "I need Embed Links permissions to send messages"
+        end
       end
     end
   end
