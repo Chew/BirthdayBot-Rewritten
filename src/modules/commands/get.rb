f1296c5f7eba2ae18bbbb38ea9a9f88d2cc1d856
Chew <chew@chew.pw>
2019-09-15 09:09:50 -0500

    prettify get command

--- src/modules/commands/get.rb
+++ src/modules/commands/get.rb
@@ -3,11 +3,35 @@ module Bot::DiscordCommands
   module Get
     extend Discordrb::Commands::CommandContainer
     command(:get) do |event, user|
-      day = Birthday.find_by(userid: user || event.user.id)
+      target = if user.nil?
+                 event.user
+               elsif !Bot::BOT.user(user.to_i).nil?
+                 Bot::BOT.user(user)
+               elsif !Bot::BOT.parse_mention(user).nil?
+                 Bot::BOT.parse_mention(user)
+               end
+
+      day = Birthday.find_by(userid: target.id)
       if day.nil?
-        event.respond "No birthday for that/you bucko"
+        begin
+          event.channel.send_embed do |embed|
+            embed.title = "Birthday Information for #{target.distinct}"
+            embed.add_field(name: "Birthday", value: "No birthday set!")
+            embed.color = 0xFF685F
+          end
+        rescue Discordrb::Errors::NoPermission
+          event.user.dm "I need Embed Links permissions to send messages"
+        end
       else
-        event.respond "yeah yeah your big day is #{day.birthday}"
+        begin
+          event.channel.send_embed do |embed|
+            embed.title = "Birthday Information for #{target.distinct}"
+            embed.add_field(name: "Birthday", value: "#{day.strftime("%B %-d, %Y")} GMT #{day.offset}")
+            embed.colour = 0x25E86F
+          end
+        rescue Discordrb::Errors::NoPermission
+          event.user.dm "I need Embed Links permissions to send messages"
+        end
       end
     end
   end
