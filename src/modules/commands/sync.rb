d852879dcb3575250f8492db00c1ab17cd4d8306
Chew <chew@chew.pw>
2019-09-23 19:47:55 -0500

    syncall every 30 mins

--- src/modules/commands/sync.rb
+++ src/modules/commands/sync.rb
@@ -4,6 +4,73 @@ module Bot::DiscordCommands
   # can use this command.
   module Sync
     extend Discordrb::Commands::CommandContainer
+    Scheduler.cron('*/30 * * * *') do
+      total = Bot::BOT.servers.count
+      m = Bot::BOT.channel(625812110781317120).send "Syncing every server's Birthdays!\nStatus: 0/#{total}"
+
+      ays = Server.all
+      sids = []
+      ays.each do |e|
+        sids.push e.serverid
+      end
+      bigbois = Bot::BOT.servers
+      bigbois.delete_if { |e, _f| !sids.include?(e) }
+
+      i = 0
+      no = 0
+      bigbois.each do |id, serv|
+        i += 1
+        server = Server.find_by(serverid: id)
+        if server.nil? || server.roleid.nil?
+          m.edit "Syncing every server's Birthdays!\nStatus: #{i+1}/#{total}"
+          break
+        end
+
+        given = 0
+        taken = 0
+        unchanged = 0
+
+        days = Birthday.all
+        uids = {}
+        days.each do |e|
+          uids[e.userid] = [e.birthday, e.time_friendly_offset]
+        end
+        goodguys = serv.members
+        goodguys.delete_if { |e| !uids.include?(e.id) }
+
+        goodguys.each do |u|
+          user = uids[u.id]
+
+          userday = user[0]
+          now = Time.now.getlocal(user[1])
+          if user[0].nil?
+            next
+            unchanged += 1
+          end
+          day = Time.new(now.year, userday.month, userday.day, 0, 0, 0, user[1]).to_i
+          day2 = (day + 86_399).to_i
+          if Time.new(now.year, now.month, now.day, now.hour, now.min, now.sec, user[1]).to_i.between?(day, day2)
+            if u.roles.include?(serv.role(server.roleid))
+              unchanged += 1
+            else
+              serv.member(user.id).add_role(server.roleid)
+              given += 1
+            end
+          else
+            if u.roles.include?(serv.role(server.roleid))
+              serv.member(user.id).remove_role(server.roleid)
+              taken += 1
+            else
+              unchanged += 1
+            end
+          end
+        end
+
+        m.edit "Syncing every server's Birthdays!\nStatus: #{i+1}/#{total}"
+      end
+      m.edit "Syncing complete. All servers synced, however #{no} servers didn't have a role."
+    end
+
     command(:sync) do |event|
       m = event.respond 'Syncing your role in this server...'
       user = Birthday.find_by(userid: event.user.id)
@@ -95,9 +162,17 @@ module Bot::DiscordCommands
         break
       end
 
+      ays = Server.all
+      sids = []
+      ays.each do |e|
+        sids.push e.serverid
+      end
+      bigbois = event.bot.servers
+      bigbois.delete_if { |e| !sids.include?(e.id) }
+
       i = 0
       no = 0
-      event.bot.servers.each do |id, serv|
+      bigbois.each do |id, serv|
         i += 1
         server = Server.find_by(serverid: id)
         if server.nil? || server.roleid.nil?
